11444ab6957150ba49b3bfb1e781f547859c2977
iamdv43 <iamdv43@gmail.com>
2025-11-09 17:58:38 -0500

    Configure gateway service

--- gateway/src/main/java/com/orderflow/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/orderflow/gateway/GatewayApplication.java
@@ -2,12 +2,24 @@ package com.orderflow.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+		System.out.println("Gateway Service is running...");
 	}
 
+	@Bean
+	public GlobalFilter customLogFilter() {
+		return (exchange, chain) -> {
+			System.out.println("Gateway received request: " + exchange.getRequest().getPath());
+			return chain.filter(exchange)
+					.then(Mono.fromRunnable(() -> System.out
+							.println("Gateway finished processing: " + exchange.getRequest().getPath())));
+		};
+	}
 }
--- user/src/main/java/com/orderflow/user/controllers/UserController.java
+++ user/src/main/java/com/orderflow/user/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class UserController {
@@ -17,23 +18,31 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/users")
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers(HttpServletRequest request) {
+        logHeaders(request);
         return userRepository.findAll();
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> addUser(@RequestBody User user) {
-
+    public ResponseEntity<User> addUser(@RequestBody User user, HttpServletRequest request) {
+        logHeaders(request);
         if (user.getUserId() == null) {
             return ResponseEntity.badRequest().build();
         }
-
         User savedUser = userRepository.save(user);
         return ResponseEntity.ok(savedUser);
     }
 
     @GetMapping("/")
-    public String home() {
-        return "User Service Home test 1";
+    public String home(HttpServletRequest request) {
+        logHeaders(request);
+        return "User Service Running...";
+    }
+
+    private void logHeaders(HttpServletRequest request) {
+        System.out.println("---- Incoming Request Headers ----");
+        request.getHeaderNames().asIterator().forEachRemaining(
+                name -> System.out.println(name + ": " + request.getHeader(name)));
+        System.out.println("----------------------------------");
     }
 }
